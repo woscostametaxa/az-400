95e0b3c7669a93e7ccc77038af588a09888d2dd3
Alexander Pajer <alexander.pajer@integrations.at>
2021-02-25 13:00:21 +0100

    tf

--- Demos/14-AzureThirdParty/Demo-02/create-app-service.tf
+++ Demos/14-AzureThirdParty/Demo-02/create-app-service.tf
@@ -1,37 +1,37 @@
 # Configure the Azure provider
-provider "azurerm" { 
-    # The "feature" block is required for AzureRM provider 2.x. 
-    # If you are using version 1.x, the "features" block is not allowed.
-    version = "~>2.0"
-    features {}
+# https://github.com/hashicorp/terraform/issues/24200
+
+provider "azurerm" {
+  version = "=1.44.0"
+  features {}
 }
 
-resource "azurerm_resource_group" "az400-t05-terraform" {
+resource "azurerm_resource_group" "az400-m14-terraform" {
     name = "az400-m014-terraform"
     location = "westeurope"
 }
 
-resource "azurerm_app_service_plan" "az400-t05-terraform" {
+resource "azurerm_app_service_plan" "az400-m14-terraform" {
     name                = "terraformPlan"
-    location            = azurerm_resource_group.az400-t05-terraform.location
-    resource_group_name = azurerm_resource_group.az400-t05-terraform.name
+    location            = azurerm_resource_group.az400-m14-terraform.location
+    resource_group_name = azurerm_resource_group.az400-m14-terraform.name
     sku {
         tier = "Standard"
         size = "S1"
     }
 }
 
-resource "azurerm_app_service" "az400-t05-terraform" {
+resource "azurerm_app_service" "az400-m14-terraform" {
     name                = "terraformApp1234"
-    location            = azurerm_resource_group.az400-t05-terraform.location
-    resource_group_name = azurerm_resource_group.az400-t05-terraform.name
-    app_service_plan_id = azurerm_app_service_plan.az400-t05-terraform.id
+    location            = azurerm_resource_group.az400-m14-terraform.location
+    resource_group_name = azurerm_resource_group.az400-m14-terraform.name
+    app_service_plan_id = azurerm_app_service_plan.az400-m14-terraform.id
 }
 
-resource "azurerm_app_service_slot" "az400-t05-terraform" {
+resource "azurerm_app_service_slot" "az400-m14-terraform" {
     name                = "terraformApp1234SlotOne"
-    location            = azurerm_resource_group.az400-t05-terraform.location
-    resource_group_name = azurerm_resource_group.az400-t05-terraform.name
-    app_service_plan_id = azurerm_app_service_plan.az400-t05-terraform.id
-    app_service_name    = azurerm_app_service.az400-t05-terraform.name
+    location            = azurerm_resource_group.az400-m14-terraform.location
+    resource_group_name = azurerm_resource_group.az400-m14-terraform.name
+    app_service_plan_id = azurerm_app_service_plan.az400-m14-terraform.id
+    app_service_name    = azurerm_app_service.az400-m14-terraform.name
 }
